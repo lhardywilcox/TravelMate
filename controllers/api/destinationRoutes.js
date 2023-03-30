2100f9c179740f47c28fbc1c9eecb2ad392cfe55
Andrey <ashesto121@gmail.com>
2023-03-29 19:59:59 -0700

    deleted files

--- controllers/api/destinationRoutes.js
+++ controllers/api/destinationRoutes.js
@@ -1,9 +1,1 @@
-const express = require("express");
-const destinationController = require("../controllers/destinationController");
 
-const router = express.Router();
-
-router.get("/", destinationController.getAll);
-router.get("/:id/activities", destinationController.getActivities);
-
-module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,1 @@
-const express = require('express');
-const destinationRoutes = require('./destinationRoutes');
 
-const router = express
