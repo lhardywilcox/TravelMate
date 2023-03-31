83f3fa4a54be7a935e03e13cb8fe58553231d28f
kate.sundquist2 <kate.sundquist2@gmail.com>
2023-03-30 18:49:04 -0700

    added user routes and finished logout

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            
+        })
+    }
+}
