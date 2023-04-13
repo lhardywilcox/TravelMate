856c13dd1b9154d129deca4ac2705b121cdc4903
LHardyWilcox <motacycaryda@mac.com>
2023-04-12 22:32:15 -0700

    working on signup process

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
 
 
 const savedRoutes = require('./city');
 
 router.use('/city', savedRoutes);
+router.use('/', userRoutes);
 
 module.exports = router;
 
